c9c6cfcfc5d35ad96375ba0eb6f3414283c35c8c
Simon J. Larsen <simonhffh@gmail.com>
2020-09-13 22:51:16 +0200

    Added get_fan_wishlist function

--- R/collection.R
+++ R/collection.R
@@ -1,4 +1,4 @@
-#' Get data from fan collection
+#' Get data for all items in fan collection
 #'
 #' @param username Username of the fan account
 #' @export
@@ -14,17 +14,16 @@ get_fan_collection <- function(username) {
     html_attr("data-blob") %>%
     fromJSON()
 
-
   collection <- unname(pagedata$item_cache$collection)
 
   fan_id <- pagedata$fan_data$fan_id
   last_token <- pagedata$collection_data$last_token
-  batch_size <- pagedata$hidden_data$batch_size
+  batch_size <- pagedata$collection_data$batch_size
 
-  more_available <- pagedata$collection_count > batch_size
+  more_available <- pagedata$collection_data$item_count > batch_size
   while(more_available) {
     req <- POST(
-      "https://bandcamp.com/api/fancollection/1/collection_items",
+      "http://bandcamp.com/api/fancollection/1/collection_items",
       body=list(fan_id=fan_id, older_than_token=last_token, count=batch_size),
       encode="json"
     )
@@ -36,4 +35,43 @@ get_fan_collection <- function(username) {
   }
 
   return(collection)
+}
+
+#' Get data for all items in fan wishlist
+#'
+#' @param username Username of the fan account
+#' @export
+#' @importFrom xml2 read_html
+#' @importFrom rvest "%>%" html_node html_nodes html_attr
+#' @importFrom jsonlite fromJSON
+#' @importFrom httr POST content
+get_fan_wishlist <- function(username) {
+  html <- read_html(paste0("http://bandcamp.com/", username, "/wishlist"))
+
+  pagedata <- html %>%
+    html_node("#pagedata") %>%
+    html_attr("data-blob") %>%
+    fromJSON()
+
+  wishlist <- unname(pagedata$item_cache$wishlist)
+
+  fan_id <- pagedata$fan_data$fan_id
+  last_token <- pagedata$wishlist_data$last_token
+  batch_size <- pagedata$wishlist_data$batch_size
+
+  more_available <- pagedata$wishlist_data$item_count > batch_size
+  while(more_available) {
+    req <- POST(
+      "http://bandcamp.com/api/fancollection/1/wishlist_items",
+      body=list(fan_id=fan_id, older_than_token=last_token, count=batch_size),
+      encode="json"
+    )
+
+    data <- content(req, "parsed")
+    wishlist <- c(wishlist, data$items)
+    more_available <- data$more_available
+    last_token <- data$last_token
+  }
+
+  return(wishlist)
 }
