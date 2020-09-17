01f57c243b5bbc345f3f1d097279ae0688719c3d
Simon J. Larsen <simonhffh@gmail.com>
2020-09-17 21:14:33 +0200

    Added get_fan_info function

---
+++ R/fan.R
@@ -0,0 +1,16 @@
+#' Get data for a fan account
+#'
+#' @export
+#' @importFrom xml2 read_html
+#' @importFrom rvest "%>%" html_node html_attr
+#' @importFrom jsonlite fromJSON
+get_fan_info <- function(username) {
+  html <- read_html(paste0("http://bandcamp.com/", username))
+
+  pagedata <- html %>%
+    html_node("#pagedata") %>%
+    html_attr("data-blob") %>%
+    fromJSON()
+
+  return(pagedata$fan_data)
+}
