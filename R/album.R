487eadf37c65b40564b102b37d533a8232a5328b
Simon J. Larsen <simonhffh@gmail.com>
2020-10-12 22:41:47 +0200

    Updated get_album_info to use new embedded json data instead

--- R/album.R
+++ R/album.R
@@ -4,45 +4,15 @@
 #'
 #' @export
 #' @importFrom xml2 read_html
-#' @importFrom rvest "%>%" html_node html_nodes html_text html_attr
+#' @importFrom rvest "%>%" html_node html_text
+#' @importFrom jsonlite fromJSON
 get_album_info <- function(album_url) {
   html <- read_html(album_url)
 
-  artist <- html %>%
-    html_node(xpath = "//*[@itemprop='byArtist']/a") %>%
-    html_text()
-
-  location <- html %>%
-    html_node("#band-name-location .location") %>%
-    html_text()
-
-  title <- html %>%
-    html_nodes(xpath = "//h2[@itemprop='name']") %>%
+  data <- html %>%
+    html_node(xpath="//script[@type='application/ld+json']") %>%
     html_text() %>%
-    trimws()
+    fromJSON
 
-  tags <- html %>%
-    html_nodes(xpath = "//*[@itemprop='keywords']") %>%
-    html_text()
-
-  pub <- html %>%
-    html_node(xpath = "//*[@itemprop='datePublished']") %>%
-    html_attr("content")
-  published <- paste(substr(pub, 1, 4), substr(pub, 5, 6),
-                     substr(pub, 7, 8), sep = "-")
-
-  description <- html %>%
-    html_node(xpath = "//*[@itemprop='description']") %>%
-    html_text()
-  description <- gsub("\\r", "", description) %>% trimws()
-
-  return(list(
-    artist = artist,
-    location = location,
-    title = title,
-    tags = tags,
-    url = album_url,
-    published = published,
-    description = description
-  ))
+  return(data)
 }
